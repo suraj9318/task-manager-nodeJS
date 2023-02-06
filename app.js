342ff1bf2859f80c40e749bec3d022f551f12044
suraj <surajmishra9318@gmail.com>
2023-02-06 21:08:58 +0530

    Controller routes and midlleware setup

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app =express();
+const tasks = require('./routes/tasks')
 
+// middleware
+app.use(express.json())
 // routes
-
-app.get('/',(req,resp)=>{
-    resp.send('Task manager')
-})
+app.use('/api/v1/tasks',tasks);
 const port = 5000;
 app.listen(port,console.log(`server is listening on port ${port}...`))

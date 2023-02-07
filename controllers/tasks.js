bbcf5de733b80914c92eedef2117c9905f8eb931
suraj <surajmishra9318@gmail.com>
2023-02-07 12:44:58 +0530

    createAPI Done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,9 +1,13 @@
+const Task = require('../models/tasks')
+
+
 const getAllTasks = (req,res)=>{
     res.send("get all tasks")
 }
 
-const createTask  = (req,res)=>{
-    res.send("create task")
+const createTask  = async(req,res)=>{
+    const task =await Task.create(req.body);
+    res.status(201).json({task});
 }
 
 const getTask  = (req,res)=>{

b538458f8ab2bd4c46bb5d10de58e082cf129ac1
suraj <surajmishra9318@gmail.com>
2023-02-09 17:59:23 +0530

    delete api done

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -40,8 +40,17 @@ const updateTask  = (req,res)=>{
     res.send("update task")
 }
 
-const deleteTask  = (req,res)=>{
-    res.send("delete task")
+const deleteTask  = async(req,res)=>{
+    try{
+        const {id : taskID} = req.params;
+        const task = await Task.findByIdAndDelete({_id : taskID})
+        if(!task){
+            return  res.status(404).json({msg : `no task found with ${req.params.id}` })
+        }
+        res.status(200).json({task})
+    }catch(error){
+        res.status(500).json({msg : err})
+    }
 }
 
 
